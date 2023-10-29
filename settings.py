51891f04bb7963c8c42261d412439be65b8c274e
sebkeil <62847324+sebkeil@users.noreply.github.com>
2023-10-29 10:01:42 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 database_connection = {
     'dbname': 'stockfetcher',
     'user': 'postgres',
-    'password': 'pAlcacer9!',
+    'password': '********',
     'host': 'localhost',    # Use the appropriate host if not localhost
     'port': '5432'         # Default PostgreSQL port, change if yours is different
 }
